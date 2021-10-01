f46334270ecc9103afb480d23706421dbac41f44
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-10-02 00:52:44 +0800

    更新備註

--- PagesNavTest/PagesNavTest/VC/MainVC.swift
+++ PagesNavTest/PagesNavTest/VC/MainVC.swift
@@ -12,15 +12,18 @@ class MainVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    
+    // 有拉 segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        // 判斷segue ID
         if segue.identifier == "segueToShowVC" {
+            // 設定目標頁類別物件
             if let destination = segue.destination as? ShowVC {
+                // 把值帶過去目標頁
                 destination.value = value
             }
         }
         
+        // 判斷segue ID
         if segue.identifier == "segueToPresentVC" {
             if let destination = segue.destination as? PresentVC {
                 destination.value = value
@@ -35,10 +38,13 @@ class MainVC: UIViewController {
     
     // 沒拉線，show
     @IBAction func clickNoShow(_ sender: Any) {
-        
+        // 看目標頁面在哪一個 storyboard
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // 跳頁目標的物件，要記得轉型
         let noShowVC = storyboard.instantiateViewController(identifier: "NoShowVC") as! NoShowVC
+        // 帶值的設定
         noShowVC.value = value
+        // 跳轉 Show 的程式
         self.navigationController?.pushViewController(noShowVC, animated: true)
         
     }
--- PagesNavTest/PagesNavTest/VC/ShowVC.swift
+++ PagesNavTest/PagesNavTest/VC/ShowVC.swift
@@ -4,6 +4,8 @@ import UIKit
 
 class ShowVC: UIViewController {
     
+    // 宣告一個“String” 的容器，前一頁就會透過此把東西傳過來
+    // 容器型別不限String，Int Float Bool 自訂類別，都可以。
     var value: String?
 
     @IBOutlet weak var tvResult: UILabel!
@@ -12,6 +14,7 @@ class ShowVC: UIViewController {
         super.viewDidLoad()
 
         if let value = value {
+            // 把值顯示出來
             tvResult.text = value
         }
         
